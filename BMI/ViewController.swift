74ee82f03f3115a7823f2ed08749d1331b2789d0
20161104587 <a20161104587@chu1e1adeiMac.local>
2018-12-28 17:21:54 +0800

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by 20161104587 on 2018/12/28.
+//  Copyright © 2018 20161104587. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
